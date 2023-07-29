0ca280107a1f1f4865a23fade3daf3839cb941b7
FirdeusGj <firdegjepali@gmail.com>
2023-07-29 13:05:56 +0200

    created the banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import './Banner.css'
+import React from "react";
+import "./Banner.css";
 
 export default function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+  }
   return (
-    <header className='banner' style={{
+    <header
+      className="banner"
+      style={{
         backgroundSize: "cover",
         backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
         backgroundPosition: "center center",
-    }}>
-        banner
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsam
+          hic nam saepe maxime voluptates doloribus sapiente, eius a aperiam
+          ipsa, repellendus quae nihil sint voluptatibus! Voluptatum dolorum
+          quas earum.`, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom" />
     </header>
-  )
+  );
 }
