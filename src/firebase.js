d69280ff28e0041050f59958386b1dea03537240
FirdeusGj <firdegjepali@gmail.com>
2023-07-31 13:43:09 +0200

    retesting

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCBH7ULu2oP7w5M11yxtleNU2OOfo841_U",
     authDomain: "netflix-clone-c2f86.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "149357014073",
     appId: "1:149357014073:web:dd0d1228bc9454820231e8"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore()
+  const auth = firebase.auth();
+
+  export { auth }
+  export default db;
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import './SignupScreen.css'
+import React, { useRef } from "react";
+import "./SignupScreen.css";
 export default function SignupScreen() {
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+  const register = (e) => {
+    e.preventDefault();
+  }
+  const signIn = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div className='signupScreen'>
-        
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit" onClick={signIn}>Sign In</button>
+        <h4>
+          <span className="signupScreen__gray">New to Netflix?</span>
+          <span className="signupScreen__link" onClick={register}>Sign Up now.</span>
+        </h4>
+      </form>
     </div>
-  )
+  );
 }
