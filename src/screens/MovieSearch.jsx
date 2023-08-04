02d529d6d5561933b4fae057b7bcdd667e7e22f7
FirdeusGj <firdegjepali@gmail.com>
2023-08-04 23:55:59 +0200

    trying to add a new section

--- src/screens/MovieSearch.jsx
+++ src/screens/MovieSearch.jsx
@@ -3,7 +3,7 @@ import "./MovieSearch.css";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import Nav from "../Nav";
-
+import notFound from "../assets/NotFound.png";
 export default function MovieSearch() {
   const { state } = useLocation();
   const [movies, setMovies] = useState([]);
@@ -11,22 +11,27 @@ export default function MovieSearch() {
   const [loading, setLoading] = useState(false);
   const [searchMovie, setSearchMovie] = useState("");
   const [moviesAvailable, setMoviesAvailable] = useState(true);
-
   async function FetchMovies(query) {
     setLoading(true);
     const { data } = await axios
-      .get(`https://www.omdbapi.com/?apikey=59e995b1&s=${searchMovie || query}`)
+      .get(
+        `https://www.omdbapi.com/?apikey=59e995b1&s=${
+          searchMovie || query
+        }&page=1`
+      )
       .catch((error) => console.log(error));
     if (data.Search) {
-      setMovies(data.Search);
+      const filteredMovies = data.Search.filter(
+        (movie) => movie.Type !== "game" && movie.Poster !== "N/A"
+      );
+      setMovies(filteredMovies);
       setMoviesAvailable(true);
     } else {
       setMoviesAvailable(false);
-      setMovies([]); // Clear the movie list if no results are found
+      setMovies([]);
     }
     setLoading(false);
   }
-
   useEffect(() => {
     if (state && state.query) {
       FetchMovies(state.query);
@@ -39,28 +44,46 @@ export default function MovieSearch() {
       FetchMovies();
     }
   };
+  // #e50914
   return (
     <>
-    <Nav/>
-    <div>
-      <input
-        type="text"
-        value={searchMovie}
-        placeholder="Movie Name"
-        onChange={(event) => setSearchMovie(event.target.value)}
-        onKeyUp={searchEnter}
-        />
-      <button onClick={() => FetchMovies(searchMovie)}>Search</button>
-      </div>
-      {!moviesAvailable ? (
-        <h1>no movies found</h1>
-      ) : loading ? <h1> Loading...</h1> : (
-        movies.slice(0, 9).map((movie) => (
-          <div className="movie">
-            <h1 className="movie__title">{movie.Title}</h1>
+      <Nav />
+      <section className="movieSearch">
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+        <div className="search__input">
+          <input
+            type="text"
+            value={searchMovie}
+            placeholder="Movie Name"
+            onChange={(event) => setSearchMovie(event.target.value)}
+            onKeyUp={searchEnter}
+          />
+          <button onClick={() => FetchMovies(searchMovie)}>Search</button>
+        </div>
+        <div className="movies__wrapper">
+          <div className="movies">
+            {!moviesAvailable ? (
+              <img className="search__notFound" src={notFound} alt="" />
+            ) : loading ? (
+              <h1> Loading...</h1>
+            ) : (
+              movies.slice(0, 9).map((movie) => (
+                <div className="movie__wrapper">
+                  <div className="movie">
+                    <img src={movie.Poster} alt="" />
+                    <h4>{movie.Year}</h4>
+                    <h1 className="movie__title">{movie.Title}</h1>
+                    <h2>{movie.Type}</h2>
+                  </div>
+                </div>
+              ))
+            )}
           </div>
-        ))
-      )}
+        </div>
+      </section>
     </>
   );
 }
