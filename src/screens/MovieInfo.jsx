ec337acb658606ad24e4947139f513a280d522b9
FirdeusGj <firdegjepali@gmail.com>
2023-08-08 13:43:46 +0200

    rendered the similar movies

--- src/screens/MovieInfo.jsx
+++ src/screens/MovieInfo.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./MovieInfo.css";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, NavigationType, useLocation, useParams } from "react-router-dom";
 import axios from "axios";
+import Nav from "../Nav";
 
 export default function MovieInfo() {
   const { id } = useParams();
@@ -29,40 +30,90 @@ export default function MovieInfo() {
       fetchSimilarMovies(state.title);
     }
   }, []);
+  const watchMovie = () => {
+    alert("This feature is not implemented yet");
+  };
   return (
-    <div>
-      <div className="movieinfo__image">
-        <img src={movie.Poster} className="movieinfo__img" />
+    <>
+      <Nav />
+      <div className="movieInfo__body">
+        <div className="movieInfo">
+          <div className="info__main">
+            <div className="movieInfo__img--wrapper">
+              <img src={movie.Poster} className="movieInfo__img" />
+            </div>
+          </div>
+          <div className="movieInfo__description">
+            <h1 className="movieInfo__h1">{movie.Title}</h1>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Plot :</span> {movie.Plot}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Genre :</span> {movie.Genre}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Year :</span> {movie.Year}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Released on : </span>
+              {movie.Released}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Runtime :</span> {movie.Runtime}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Writer :</span> {movie.Writer}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Language :</span>{" "}
+              {movie.Language}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Country :</span> {movie.Country}
+            </p>
+            <p className="movieInfo__para">
+              <span className="movieInfo__span">Rating :</span>{" "}
+              {movie.imdbRating}
+            </p>
+            <div className="movieInfo__buttons">
+              <button onClick={watchMovie} className="movieInfo__button">
+                Watch
+              </button>
+              <Link to="/search" className="movieInfo__link">
+                <button className="movieInfo__button--link">
+                  Back to search
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="movieInfo__similar">
+          <h1>Similar movies</h1>
+          <div className="similar__wrapper">
+            {similarMovies
+              .filter((elem) => elem.imdbID !== id)
+              .slice(0, 4)
+              .map((recommend) => (
+                <>
+                  <div className="similar__movies" key={recommend.imdbID}>
+                    <Link
+                    className="similar__link"
+                      to={`/search/${recommend.imdbID}`}
+                      state={{ title: recommend.title || state.query }}
+                      key={recommend.imdbID}
+                    >
+                      <img
+                        src={recommend.Poster}
+                        alt=""
+                        className="similar__img"
+                      />
+                    </Link>
+                  </div>
+                </>
+              ))}
+          </div>
+        </div>
       </div>
-      <div className="movieinfo__description">
-        <h1 className="movieinfo__h1">Title : {movie.Title}</h1>
-        <h2 className="movieinfo__h2">About :</h2>
-        <p className="movieinfo__para">Plot : {movie.Plot}</p>
-        <p className="movieinfo__para">Genre : {movie.Genre}</p>
-        <p className="movieinfo__para">Year : {movie.Year}</p>
-        <p className="movieinfo__para">Released on : {movie.Released}</p>
-        <p className="movieinfo__para">Runtime : {movie.Runtime}</p>
-        <p className="movieinfo__para">Writer : {movie.Writer}</p>
-        <p className="movieinfo__para">Language : {movie.Language}</p>
-        <p className="movieinfo__para">Country : {movie.Country}</p>
-        <p className="movieinfo__para">Rating : {movie.imdbRating}</p>
-      </div>
-      <div>
-        {similarMovies
-          .filter((elem) => elem.imdbID !== id)
-          .slice(0, 4)
-          .map((recommend) => (
-            <Link
-              to={`/search/${recommend.imdbID}`}
-              state={{ title: recommend.title || state.query }}
-              key={recommend.imdbID}
-            >
-              <div className="similar__movies" key={recommend.imdbID}>
-                <img src={recommend.Poster} alt="" className="similar__img" />
-              </div>
-            </Link>
-          ))}
-      </div>
-    </div>
+    </>
   );
 }
